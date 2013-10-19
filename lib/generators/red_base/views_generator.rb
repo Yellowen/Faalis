92bb0eb90e3b183539d9b5d7fe10d5cb6cb78ea7
Sameer Rahmani <lxsameer@gnu.org>
2013-10-19 11:50:01 +0330

    Views generator added

---
+++ lib/generators/red_base/views_generator.rb
@@ -0,0 +1,21 @@
+module RedBase
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../../app/', __FILE__)
+
+      desc "Copy all templates to and assets to main application"
+
+      def copy_views_file
+        directory "views/red_base", "app/views/red_base"
+      end
+
+      def copy_assets_file
+        directory "assets/javascripts/red_base", "app/assets/javascripts/red_base"
+        directory "assets/stylesheets/red_base", "app/assets/stylesheets/red_base"
+        directory "assets/images/red_base", "app/assets/images/red_base"
+
+      end
+
+    end
+  end
+end
