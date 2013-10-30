2f8c524e0cf1306e9563d0724a81e94c4c7a8507
Sameer Rahmani <lxsameer@gnu.org>
2013-10-30 17:03:08 +0330

    selected functionlity and view on double click added to table-list

--- app/assets/javascripts/red_base/dashboard/plugins/table-list.js
+++ app/assets/javascripts/red_base/dashboard/plugins/table-list.js
@@ -1,16 +1,20 @@
 $(function(){
-    $(".table-list ul li").on("click", function(event) {
+    $(".table-list ul li").on("dblclick", function(event) {
         var details = $(this).find(".details");
         $(".table-list ul li .details").not($(details)).slideUp();
+        $(".table-list ul li i").not($(this).find(".handle")).removeClass("fa-rotate-90");
         if ($(details).is(":visible")) {
-            $(this).find("i").removeClass("fa-rotate-90");
+            $(this).find(".handle").removeClass("fa-rotate-90");
         }
         else {
-            if ($(this).has(".details")) {
-                $(this).find("i").addClass("fa-rotate-90");
+            if ($(details).length !== 0) {
+                $(this).find(".handle").addClass("fa-rotate-90");
             }
         }
         $(details).slideToggle();
 
+    });
+    $(".table-list ul li").on("click", function(event){
+        $(this).toggleClass("selected");
     });
 });
