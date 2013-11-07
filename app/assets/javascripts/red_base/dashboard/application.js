79f80e65e417e9709b0e0c667681d4f5255ffb68
Sameer Rahmani <lxsameer@gnu.org>
2013-11-07 16:50:23 +0330

    list-view template ltr bug fixed

--- app/assets/javascripts/red_base/dashboard/application.js
+++ app/assets/javascripts/red_base/dashboard/application.js
@@ -29,12 +29,6 @@ window.Dashboard = Ember.Application.create({
 window.Modules = [];
 window.ErrorQueue = [];
 
-window.is_ltr = function(){
-    if($("html").attr("dir") == "ltr") {
-        return true;
-    }
-    return false;
-};
 //= require_tree .
 $(function(){
     $(document).foundation();
