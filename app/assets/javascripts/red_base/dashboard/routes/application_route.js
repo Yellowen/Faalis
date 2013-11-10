8031dec689d81da7677e261d84e498833ae5dac0
Sameer Rahmani <lxsameer@gnu.org>
2013-11-10 15:47:49 +0330

    application route reverted

--- app/assets/javascripts/red_base/dashboard/routes/application_route.js
+++ app/assets/javascripts/red_base/dashboard/routes/application_route.js
@@ -2,13 +2,6 @@
 Dashboard.ApplicationRoute = Ember.Route.extend({
     setupController: function(controller, model){
         var that = this;
-        /*Ember.$.getJSON("/<%= RedBase::Engine.dashboard_namespace.to_s %>/modules.json")
-            .done(function(data) {
-                that.controllerFor('modules').set('model', data.modules);
-            })
-            .fail(function(data){
-                console.log("Fix me");
-            });*/
         get_modules();
         this.controllerFor('modules').set('model', Modules);
     }
