d91e923669aa1d4361a20bb97bcbd568120f8436
Behnam Ahmad Khan Beigi <b3hnam@gnu.org>
2014-01-06 15:44:02 +0330

    edit profile modified

--- app/assets/javascripts/red_base/dashboard/modules/auth/profile.js
+++ app/assets/javascripts/red_base/dashboard/modules/auth/profile.js
@@ -30,11 +30,28 @@ Profile.config(["$routeProvider", function($routeProvider){
 
 
 
-Profile.controller("ProfileController", ["$scope", "Restangular","gettext", "catch_error",
-                                         function($scope, API, gettext, catch_error){
-                                             API.one("profile").get().then(
-                                                 function(data){
-                                                     $scope.profle = data;
-                                                 });
+Profile.controller("ProfileController",  ["$scope","Restangular","$location" ,"$routeParams", "gettext", "catch_error", function($scope, API, $location , $routeParams, gettext, catch_error){
+    var obj = API.one("profile").get()
+            .then(function(data){
+                $scope.first_name = data.first_name;
+                $scope.last_name = data.last_name;
+                $scope.email = data.email;
 
-                                         }]);
+            });
+    $scope.save = function() {
+        var user = {
+            first_name: $scope.first_name,
+            last_name: $scope.last_name,
+            email: $scope.email
+        };
+        API.one("profile").patch(user).then(function(){
+            success_message(gettext("Profile updated successfully."));
+        }).catch(catch_error);
+    };
+
+    $scope.cancel = function(){
+        $(".form input").val("");
+        $location.path("/");
+    };
+
+}]);
