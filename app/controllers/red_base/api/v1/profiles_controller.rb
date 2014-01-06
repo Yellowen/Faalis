a19cdd430a89176081aa29759b688c49ee38527e
Behnam Ahmad Khan Beigi <b3hnam@gnu.org>
2014-01-06 17:38:41 +0330

    user profile modified

--- app/controllers/red_base/api/v1/profiles_controller.rb
+++ app/controllers/red_base/api/v1/profiles_controller.rb
@@ -5,11 +5,25 @@ module RedBase
     def show
       if user_signed_in?
         @user = current_user
+        respond_with(@user)
       end
     end
 
     def update
+      if user_signed_in?
+        @user = current_user
+        user_fields = {
+          :first_name => params[:first_name],
+          :last_name => params[:last_name],
+          :email => params[:email],
+        }
+        if @user.update_without_password(user_fields)
+          respond_with(@user)
+        else          respond_to do |format|
+            format.json { render :json => {:fields => @user.errors}, :status => :unprocessable_entity }
+          end
+        end
+      end
     end
-
   end
 end
