614c3997b4a32e8323fb2280f595b782a42453c7
Behnam Ahmad Khan Beigi <b3hnam@gnu.org>
2014-01-16 17:52:27 +0330

    mailboxer modified

--- app/assets/javascripts/faalis/dashboard/modules/conversations.js
+++ app/assets/javascripts/faalis/dashboard/modules/conversations.js
@@ -1,21 +1,21 @@
 /* -----------------------------------------------------------------------------
-    Red Base - Basic website skel engine
-    Copyright (C) 2012-2013 Yellowen
+   Red Base - Basic website skel engine
+   Copyright (C) 2012-2013 Yellowen
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
------------------------------------------------------------------------------ */
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+   ----------------------------------------------------------------------------- */
 var Conversation = angular.module("Conversation",["ListView", "Errors"]);
 
 
@@ -41,6 +41,27 @@ Conversation.config(["$routeProvider", function($routeProvider){
 
 Conversation.controller("ConversationControllerIndex",["$scope", "Restangular", "gettext", "catch_error", "$routeParams","$location", function($scope, API, gettext, catch_error, $routeParams, $location){
     $scope.details_template = template("conversations/details");
+    $scope.details_templates = template("conversations/details");
+
+    $scope.on_trash = function(conversations){
+        return conversations;
+        var query = [];
+        conversations.forEach(function(conversation){
+            query.push(conversation.id);
+        });
+
+        API.all("conversations",query.join(",")).post()
+            .then(function(data) {
+
+                $scope.conversations = _.filter($scope.Conversations, function(x){
+                    return !(query.indexOf(x.id) != -1);
+                });
+                success_message(data.msg);
+            })
+            .catch(catch_error);
+
+    };
+
     var type;
     switch ($routeParams.type){
     case "inbox":
@@ -80,63 +101,6 @@ Conversation.controller("ConversationControllerIndex",["$scope", "Restangular", 
             route: "#/conversations/trash"
         }
     ];
-}]);
-
-Conversation.controller("ConversationControllerNew", ["$scope", "Restangular", "gettext", "catch_error", "$routeParams", "$location", function($scope, API, gettext, catch_error, $routeParams, $location){
-    $scope.obj_id = null;
-    if("id" in $routeParams){
-        $scope.obj_id = $routeParams.id;
-        var obj = API.one("conversations", $scope.obj_id).get()
-                .then(function(data){
-                    $scope.recipients = data.recipients;
-                    $scope.subject = data.subject;
-                    $scope.body = data.body;
-                });
-    }
-
-    $scope.save = function() {
-        var conversation = {conversation: {
-            recipients: $scope.recipients,
-            subject: $scope.subject,
-            body: $scope.body
-        }};
-        if ($scope.obj_id){
-            API.one("conversations",$scope.obj_id).post(conversation).then(function(){
-                success_message(gettext("Message sent successfully."));
-                $location.path("conversations/inbox");
-                });
-        }else{
-            API.all("conversations").post(conversation).then(function(){
-                success_message(gettext("Message sent Successfully"));
-                $location.path("conversations/inbox");
-            }).catch(catch_error);
-        }
-    };
-
-    $scope.cancel = function(){
-        $(".form input").val("");
-        $location.path("conversations/index");
-    };
-
-
-    $scope.on_trash = function(conversations){
-        var query = [];
-        conversations.forEach(function(conversation){
-            query.push(conversation.id);
-        });
-
-        API.all("conversations",query.join(",")).post()
-            .then(function(data) {
-
-                $scope.conversations = _.filter($scope.Conversations, function(x){
-                    return !(query.indexOf(x.id) != -1);
-                });
-                success_message(data.msg);
-            })
-            .catch(catch_error);
-
-    };
-
 
     $scope.on_untrash = function(conversations){
         var query = [];
@@ -154,5 +118,45 @@ Conversation.controller("ConversationControllerNew", ["$scope", "Restangular", "
             })
             .catch(catch_error);
     };
+
+
+}]);
+
+Conversation.controller("ConversationControllerNew", ["$scope", "Restangular", "gettext", "catch_error", "$routeParams", "$location", function($scope, API, gettext, catch_error, $routeParams, $location){
+    $scope.obj_id = null;
+    if("id" in $routeParams){
+        $scope.obj_id = $routeParams.id;
+        var obj = API.one("conversations", $scope.obj_id).get()
+            .then(function(data){
+                $scope.recipients = data.recipients;
+                $scope.subject = data.subject;
+                $scope.body = data.body;
+            });
+    }
+
+    $scope.save = function() {
+        var conversation = {conversation: {
+            recipients: $scope.recipients,
+            subject: $scope.subject,
+            body: $scope.body
+        }};
+        if ($scope.obj_id){
+            API.one("conversations",$scope.obj_id).post(conversation).then(function(){
+                success_message(gettext("Message sent successfully."));
+                $location.path("conversations/inbox");
+            });
+        }else{
+            API.all("conversations").post(conversation).then(function(){
+                success_message(gettext("Message sent Successfully"));
+                $location.path("conversations/inbox");
+            }).catch(catch_error);
+        }
+    };
+
+    $scope.cancel = function(){
+        $(".form input").val("");
+        $location.path("conversations/index");
+    };
+
 
 }]);
