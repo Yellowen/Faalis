214e53cb270e3989f2594b5414da32bd8023f314
Sameer Rahmani <lxsameer@gnu.org>
2014-03-15 14:05:13 +0330

    string-field extended to support extra links

--- app/assets/javascripts/faalis/dashboard/modules/fields/string.js
+++ app/assets/javascripts/faalis/dashboard/modules/fields/string.js
@@ -9,6 +9,19 @@ String_.directive('stringField', ["$filter", "gettext", function($filter, gettex
         var ltr = is_ltr();
         scope.element_id = "id_" + scope.field;
         scope.msg_element_id = "id_" + scope.field + "_msg";
+        scope.show_help_btn = false;
+        scope.show_help_text = true;
+        // Decide to see help text or help button
+        if (scope.options === undefined) {
+            scope.options = {};
+        }
+
+        if ("help_text" in scope.options) {
+            if ("show_help_btn" in scope.options && scope.options.show_help_btn === true) {
+                scope.show_help_btn = true;
+                scope.show_help_text = false;
+            }
+        }
 
         if (scope.on_change !== undefined) {
             // Watch event changes
@@ -18,6 +31,24 @@ String_.directive('stringField', ["$filter", "gettext", function($filter, gettex
             }, true);
         }
 
+        scope.help_btn_clicked = function() {
+            if ("show_help_callback" in scope.options) {
+                scope.options.show_help_btn();
+            }
+            else {
+                scope.show_help_text = !scope.show_help_text;
+            }
+        };
+
+        scope.have = function(ob, element) {
+            if (element in ob) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        };
+
     }
     // Actual object of <string-field> directive
     return {
@@ -25,7 +56,15 @@ String_.directive('stringField', ["$filter", "gettext", function($filter, gettex
         replace: true,
         restrict: "E",
         scope: {
+            // you can control the buttons and help
+            // message of field here. for example you can use `help_text` to
+            // show an small help under the control and you can set `show_help_btn`
+            // to true to show an help button.
+            options: '=',
+
+            // Css classes to apply on element
             cssClasses: '=cssClass',
+
             // A call back to pass to field ng-change directive
             on_change: "@onChange",
             // fieldname
