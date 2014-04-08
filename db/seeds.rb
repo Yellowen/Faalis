4b9103cb2174c62ee667b5ffc83cbd191342eaaf
Sameer Rahmani <lxsameer@gnu.org>
2014-04-08 22:41:27 +0430

    minor changes in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,18 @@
 
 ModelDiscovery::Engine.load_seed
 
-Faalis::Group.create({:name => "Admin"})
-Faalis::Group.create({:name => "Guest", :id => 2})
+Faalis::Group.create({:name => 'Admin'})
 
-Faalis::User.create({
-                       :email => "admin@example.com",
-                       :group_id => 1,
-                       :password => "123123123",
-                       :password_confirmation => "123123123"})
+Faalis::User.create({:email => 'admin@example.com',
+                     :group_id => 1,
+                     :password => '123123123',
+                     :password_confirmation => '123123123'})
 
-Faalis::User.create({:email => "user@example.com",
-            :group_id => 999,
-            :password => "123123123",
-            :password_confirmation => "123123123"})
+
+Faalis::Group.create({:name => 'Guest', :id => 2})
+
+
+Faalis::User.create({:email => 'user@example.com',
+            :group_id => 2,
+            :password => '123123123',
+            :password_confirmation => '123123123'})
