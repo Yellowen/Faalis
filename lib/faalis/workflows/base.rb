cf388db54c1c372f118a448b2e103f2c041a868e
Sameer Rahmani <lxsameer@gnu.org>
2014-05-03 23:36:03 +0430

    workflow base done

--- lib/faalis/workflows/base.rb
+++ lib/faalis/workflows/base.rb
@@ -6,62 +6,63 @@ module Faalis
 
       attr_reader :id
 
+      @@default_values = {:title => self.class.to_s.underscore,
+                          :icon => nil,
+                          :image => nil,
+                          :permissions => []}
+
       def initialize(model_instance)
         @id = model_instance.id
       end
 
       # class method to set the title of current workflow
       def self.title(name)
-        instance_variable_set(:@title, name)
+        define_method :title do
+           name || self.to_s.underscore
+        end
       end
 
-      def title
-        @title || self.class.to_s.underscore
-      end
 
       # Set the current workflow icon if any
       def self.icon(icon)
-        instance_variable_set(:@icon, icon)
+        define_method :icon do
+           icon
+        end
       end
 
-      # Getter for workflow icon
-      def icon
-        return @icon if defined? :@icon
-        nil
-      end
 
       def icon?
-        return true if defined? :@icon
+        return true if defined? :icon
         false
       end
 
 
       # Set the current workflow image if any
       def self.image(image)
-        instance_variable_set(:@image, image)
-      end
-
-      # Getter for the current workflow image
-      def image
-        return @image if defined? :@image
-        nil
+        define_method :image do
+           image
+        end
       end
 
       def image?
-        return true if defined? :@image
+        return true if defined? :image
         false
       end
 
       # Use this class method to add any permission that is neeeded
       # for this workflow to work
       def self.permissions(*args)
-        instance_variable_set(:@permissions, args)
+        define_method :permissions do
+           args || []
+        end
       end
 
-      # permissions getter
-      def permissions
-        return @permissions if defined? @permissions
-        []
+      def method_missing(m, *args, &block)
+        if @@default_values.include? m
+          @@default_values[m]
+        else
+          super
+        end
       end
 
     end
