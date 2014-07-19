ee8f0c57f2fe8af4b1b9b31d812476aaa59ff38c
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2014-07-20 03:50:27 +0430

    base structure of image directive added

---
+++ app/assets/javascripts/faalis/dashboard/modules/fields/.#image.js
@@ -0,0 +1,1 @@
+yottanami@localhost.14467:1405803552
--- app/assets/javascripts/faalis/dashboard/modules/fields/image.js
+++ app/assets/javascripts/faalis/dashboard/modules/fields/image.js
@@ -3,3 +3,44 @@ var Image_ = angular.modulde("DatetimeField", []);
 /*
  * <image-field></image-field>
 */
+
+Image_.directive('imageField',["gettext", function(gettext){
+    function link(scope, element, attrs){
+        var ltr = is_ltr();
+        scope.element_id = "id_" + scope.field;
+        scope.msg_element_id = "id_" + scope.field + "_msg";
+
+        if (scope.timepicker === undefined) {
+            scope.timepicker = true;
+        }
+        if (scope.on_change !== undefined) {
+            // Watch event changes
+            scope.$watch("model", function(newv, oldv, $scope) {
+                // TODO: maybe we should pass locals to $eval
+                scope.$parent.$eval(scope.on_change);
+            }, true);
+        }
+    }
+    return {
+        templateUrl: template("fields/datetime/image"),
+        replace: true,
+        restrict: "E",
+        transclude: true,
+        scope: {
+            // disable timepicker
+            timepicker: "&",
+
+            cssClasses: '=cssClass',
+            // A call back to pass to field ng-change directive
+            on_change: "@onChange",
+            // fieldname
+            field: "=fieldName",
+            // Does this field is required
+            required: "=",
+            // Actual Angularjs ng-model
+            model: '='
+        },
+        link: link
+    };
+
+}]);
