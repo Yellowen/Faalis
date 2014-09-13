47f52c8f2ea92d3bac74aa41e960d7eacc76c9da
Sameer Rahmani <lxsameer@gnu.org>
2014-09-13 17:39:32 +0430

    permissions discovery and router modules refactored

--- lib/faalis/route.rb
+++ lib/faalis/route.rb
@@ -1,22 +1,27 @@
+
 module Faalis
   class Routes
-    def self.define_api_routes(routes=Rails.application.routes)
+    # This class method will add `Faalis` routes to host application
+    # Router
+    def self.define_api_routes(routes = Rails.application.routes)
 
       routes.draw do
         scope 'module'.to_sym => 'faalis' do
-          namespace :api, :defaults => {:format => :json} do
+          # TODO: Add a dynamic solution for formats
+          namespace :api, defaults: {format: :json} do
             namespace :v1 do
-              get 'permissions', :to => 'permissions#index'
-              get 'permissions/user', :to => 'permissions#user_permissions'
-              resources :groups, :except => [:new]
-              resources :users, :except => [:new]
-              resource :profile, :except => [:new, :destroy]
+              get 'permissions', to: 'permissions#index'
+              get 'permissions/user', to: 'permissions#user_permissions'
+              resources :groups, except: [:new]
+              resources :users, except: [:new]
+              resource :profile, except: [:new, :destroy]
 
               get 'logs' => 'logs#index'
             end
           end
         end
       end
+
     end
   end
 end
