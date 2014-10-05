905828da98fb8dd77d28c97cfb14b6e702005cca
Sameer Rahmani <lxsameer@gnu.org>
2014-10-06 00:04:49 +0330

    mongoid option added to install_all generator

--- lib/generators/faalis/install_all_generator.rb
+++ lib/generators/faalis/install_all_generator.rb
@@ -22,15 +22,17 @@ module Faalis
     class InstallAllGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
 
-      desc "Copy all the necessary files to use Faalis (migrations included)"
+      class_option :mongoid, type: :boolean, default: false, desc: 'Use mongoid with faalis'
+
+      desc 'Copy all the necessary files to use Faalis (migrations included)'
       hook_for :install_generator
 
       def copy_migrations
-        rake("faalis:install:migrations")
+        rake('faalis:install:migrations') unless options[:mongoid]
       end
 
       def copy_install
-        invoke "faalis:install"
+        invoke 'faalis:install'
       end
 
     end
