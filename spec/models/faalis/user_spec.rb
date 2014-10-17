201cdd0efb3039cea66c41b91559b8dfe04e4175
Sameer Rahmani <lxsameer@gnu.org>
2014-10-17 20:37:10 +0330

    user spec improved

--- spec/models/faalis/user_spec.rb
+++ spec/models/faalis/user_spec.rb
@@ -1,16 +1,13 @@
 require 'spec_helper'
 
-RSpec.describe Faalis::User, :type => :model do
+describe Faalis::User, :type => :model do
 
-  before do
-    @passwird = Faker::Internet.password(8)
-
-    @admin_group = create(:faalis_group, name: 'Admin', role: 'admin')
-    @guest_group = create(:faalis_group)
-  end
+  let(:fake_passwird) { Faker::Internet.password(8) }
+  let(:admin_group) { create(:faalis_group, name: 'Admin', role: 'admin') }
+  let(:guest_group) { create(:faalis_group) }
 
   it 'has a valid factory' do
-    expect(build(:faalis_user, password: @password)).to be_valid
+    expect(build(:faalis_user, password: fake_password)).to be_valid
   end
 
   it 'should not be valid without a password' do
@@ -18,20 +15,20 @@ RSpec.describe Faalis::User, :type => :model do
   end
 
   it 'should not be valid without of a valid email' do
-    expect(build(:faalis_user, password: @password, email: '')).not_to be_valid
+    expect(build(:faalis_user, password: fake_password, email: '')).not_to be_valid
     expect(build(:faalis_user,
-                 passowrd: @password,
+                 passowrd: password,
                  email: 'some_email')).not_to be_valid
   end
 
   it 'should be in "Guest" group if no group provided' do
-    user = create(:faalis_user)
+    user = create(:faalis_user, password: fake_password)
     expect(user.groups.size).to eq(1)
     expect(user.groups.first).to be_a_kind_of(Faalis::Group)
   end
 
   it 'should have a functional many to many to group' do
-    user = build(:faalis_user, groups: [@admin_group])
+    user = build(:faalis_user, groups: [admin_group])
     expect(user.groups.size).to eq(1)
     expect(user.groups.first).to be_a_kind_of(Faalis::Group)
   end
