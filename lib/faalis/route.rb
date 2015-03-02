5ca439576c8cd7933ef7110d5d89de5caac40d75
Sameer Rahmani <lxsameer@gnu.org>
2015-03-02 23:13:48 +0330

    `Faalis::Routes` bug fixed.
    Rails 4.2 broke the router module. so we had to change our routes
    api a little bit. From now on `define_api_routes` pass a router
    object to its block that we have to use to create new routes.
    
    For example:
    
       Faalis::Routes.define_api_routes do |r|
        r.resources :albums, except: [:new]
       end

--- lib/faalis/route.rb
+++ lib/faalis/route.rb
@@ -10,31 +10,30 @@ module Faalis
     # This class method will add `Faalis` routes to host application
     # Router
     def self.define_api_routes(routes: Rails.application.routes,
-                               version: :v1,
-                               &block)
+                               version: :v1)
 
       routes.draw do
         # TODO: Add a dynamic solution for formats
-        namespace :api, defaults: {format: :json} do
+        namespace :api, defaults: { format: :json } do
           namespace version do
-
             # Call user given block to define user routes
             # inside this namespace
-            block.call if block_given?
+            yield self if block_given?
+
           end
         end
 
         scope 'module'.to_sym => 'faalis' do
           # TODO: Add a dynamic solution for formats
-          namespace :api, defaults: {format: :json} do
+          namespace :api, defaults: { format: :json } do
             namespace version do
-              get 'permissions', to: 'permissions#index'
+              get 'permissions',      to: 'permissions#index'
               get 'permissions/user', to: 'permissions#user_permissions'
-              resources :groups, except: [:new]
-              resources :users, except: [:new]
-              resource :profile, except: [:new, :destroy]
+              resources :groups,      except: [:new]
+              resources :users,       except: [:new]
+              resource :profile,      except: [:new, :destroy]
 
-              get 'logs' => 'logs#index'
+              get 'logs', to: 'logs#index'
             end
 
           end
