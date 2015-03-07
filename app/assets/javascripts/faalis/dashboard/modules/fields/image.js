2c0f0695b6f81d9435ae52eefdcbd8325e11ce27
Sameer Rahmani <lxsameer@gnu.org>
2015-03-08 01:22:20 +0330

    Image filed improved to contain file name and type

--- app/assets/javascripts/faalis/dashboard/modules/fields/image.js
+++ app/assets/javascripts/faalis/dashboard/modules/fields/image.js
@@ -4,33 +4,40 @@ var Image_ = angular.module("ImageField",[]);
  * <image-field></image-field>
 */
 
-Image_.directive('imageField',["gettext",function(gettext){
-    function link(scope, element, attrs,ngModelController){
+Image_.directive('imageField',["gettext", "$parse", function(gettext, $parse){
+    function link(scope, element, attrs, ngctrl){
+        console.group("ImageField");
         scope.element_id = "id_" + scope.field;
         scope.msg_element_id = "id_" + scope.field + "_msg";
 
         function updateModel(event){
-          if(event.target && event.target.files){
-            var file = event.target.files[0];
-            var reader = new FileReader();
-            reader.onload = function(e){
-                scope.image = e.target.result;
-            };
-            reader.readAsDataURL(file);
-          }
+            if(event.target && event.target.files){
+              var file = event.target.files[0];
+              var reader = new FileReader();
+              reader.onload = function(e){
+                  var image = e.target.result;
+                  console.log("IMAGE DATA: %s", image);
+                  ngctrl.$setViewValue({filename: file.name,
+                                        image_data: image,
+                                        content_type: file.type});
+                  ngctrl.$render();
+
+              };
+                reader.readAsDataURL(file);
+            }
         }
 
-        element.on('change',updateModel)
+        element.on('change',updateModel);
 
+        console.groupEnd();
     }
     return {
         templateUrl: template("fields/image/image"),
         //getting deprecated
         //replace: true,
         restrict: "E",
-        require: '?ngModel', // get a hold of NgModelController
+        require: 'ngModel', // get a hold of NgModelController
         //transclude: true,
-
         link: link
     };
 
