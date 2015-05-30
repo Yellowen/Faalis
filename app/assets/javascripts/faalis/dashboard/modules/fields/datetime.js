799173ead50ac5c09a5d7f922a7818c3acf0871a
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-05-30 15:17:48 +0430

    binding added to datetime field

--- app/assets/javascripts/faalis/dashboard/modules/fields/datetime.js
+++ app/assets/javascripts/faalis/dashboard/modules/fields/datetime.js
@@ -8,6 +8,7 @@ Datetime_.directive('datetimeField', ["$filter", "gettext",  function($filter, g
 
     function link(scope, element, attrs){
         var ltr = is_ltr();
+        var ltr = true;
         var locale = (ltr) ? 'en' : 'fa';
         scope.element_id = "id_" + scope.field;
         //TODO: change the find selector to use ID
@@ -31,12 +32,9 @@ Datetime_.directive('datetimeField', ["$filter", "gettext",  function($filter, g
 
         });
 
-        scope.$watch('fake_model', function(x, y) {
-        });
+        element.find('div.date input').bind('blur keyup change', function(){
+          scope.model = element.find('div.date input').val();
 
-        scope.$watch('model', function(x, y){
-            console.log('xxxxxxxxxxxxxx');
-            console.log(x);
         });
     }
     // Actual object of <datetime-field> directive
