bfee90ede24528378c31233b157cb708148c01ca
Sameer Rahmani <lxsameer@gnu.org>
2015-07-28 16:19:38 +0430

    user views bugs fixed

--- config/routes.rb
+++ config/routes.rb
@@ -17,23 +17,15 @@ Faalis::Engine.routes.draw do
 
   localized_scop do
     in_dashboard do
-      get  'auth/groups/new', to: 'groups#new'
-      get  'auth/groups/:id', to: 'groups#edit', as: 'auth_groups_edit'
-      post 'auth/groups',     to: 'groups#create'
-      put  'auth/groups/:id', to: 'groups#update', as: 'auth_groups_update'
-
-      get  'auth/users',     to: 'users#index'
-      get  'auth/users/new', to: 'users#new'
-      get  'auth/users/:id', to: 'users#show', as: 'auth_users_show'
-      get  'auth/users/:id/edit', to: 'users#edit', as: 'auth_users_edit'
-      post 'auth/users',     to: 'users#create'
-      put  'auth/users/:id', to: 'users#update', as: 'auth_users_update'
-      get('auth/users/:id/password', to: 'users#edit_password',
-          as: 'auth_users_edit_password')
-
-      patch 'auth/users/:id/password', to: 'users#update_password'
-      delete  'auth/users/:id', to: 'users#destroy', as: 'auth_users_destroy'
-
+      scope :auth do
+        resources :groups, as: 'auth_groups'
+        resources :users, as: 'auth_users' do
+          member do
+            get   'password', to: 'users#edit_password', as: 'auth_users_edit_password'
+            patch 'password', to: 'users#update_password'
+          end
+        end
+      end
       get '', to: 'dashboard#index', as: 'index'
       get '/404', to: 'dashboard#not_found', as: 'not_found'
     end
