0da3413ab17739a4eb506b5302c8e09b6e408529
Sameer Rahmani <lxsameer@gnu.org>
2015-08-12 15:35:15 +0430

    index_dsl basic functionality done

--- app/helpers/faalis/dashboard_helper.rb
+++ app/helpers/faalis/dashboard_helper.rb
@@ -6,5 +6,9 @@ module Faalis
       time.strftime("%Y-%m-%d %H:%M")
     end
 
+    def get_url(route_name, id = nil, engine = Rails.application)
+      return engine.routes.url_helpers.send(route_name, id.to_s) unless id.nil?
+      engine.routes.url_helpers.send(route_name)
+    end
   end
 end
--- lib/faalis/dashboard/index_dsl.rb
+++ lib/faalis/dashboard/index_dsl.rb
@@ -5,13 +5,10 @@ module Faalis::Dashboard
 
     # The actual action method of a dashboard controller
     def index
-      puts "<<<<<<<<<" * 100, _fields
       authorize model
-      result = fetch_index_objects
-      instance_variable_set("@#{plural_name}", result)
 
-      @index_fields = _fields || model.column_names
-      @resources    = result
+      fetch_and_set_result
+      setup_named_routes
 
       return if _override_views.include? :index
       render 'faalis/dashboard/resource/index'
@@ -41,7 +38,60 @@ module Faalis::Dashboard
         nil
       end
 
+      def _route_name
+        nil
+      end
 
+      def _route_engine
+        Rails.application
+      end
+
+      def guess_index_route(scope  = 'dashboard')
+        scope_ = "#{scope}_"
+        "#{scope_}#{controller_name}_path"
+      end
+
+      def guess_show_route(scope  = 'dashboard')
+        scope_        = "#{scope}_"
+        resource_name = controller_name.singularize.underscore
+        "#{scope_}#{resource_name}_path"
+      end
+
+      def guess_edit_route(scope  = 'dashboard')
+        scope_        = "#{scope}_"
+        resource_name = controller_name.singularize.underscore
+        "edit_#{scope_}#{resource_name}_path"
+      end
+
+      def guess_new_route(scope  = 'dashboard')
+        scope_        = "#{scope}_"
+        resource_name = controller_name.singularize.underscore
+        "new_#{scope_}#{resource_name}_path"
+      end
+
+      def guess_edit_route(scope  = 'dashboard')
+        scope_        = "#{scope}_"
+        resource_name = controller_name.singularize.underscore
+        "edit_#{scope_}#{resource_name}_path"
+      end
+
+      def fetch_and_set_result
+        result = fetch_index_objects
+        instance_variable_set("@#{plural_name}", result)
+
+        @index_fields = _fields || model.column_names
+        @resources    = result
+      end
+
+      def setup_named_routes
+        @engine      = _route_engine
+        @index_route = guess_index_route
+        @new_route   = guess_new_route
+        @show_route  = guess_show_route
+        @edit_route  = guess_edit_route
+      end
+
+    # The actual DSL for index ages
     module ClassMethods
       def index_fields(*fields)
         define_method(:_fields) do
@@ -49,6 +99,20 @@ module Faalis::Dashboard
         end
 
         private :_fields
+      end
+
+      def route_engine(name, &block)
+        define_method(:_route_engine) do
+          return block.call if block_given?
+          name
+        end
+      end
+
+      def route_scope(name, &block)
+        define_method(:_route_scope) do
+          return block.call if block_given?
+          name
+        end
       end
     end
   end
