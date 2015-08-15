d8840170d6e922c506f24e01548c0670f9be1c33
Sameer Rahmani <lxsameer@gnu.org>
2015-08-15 19:04:41 +0430

    generic new form added to dashboard section

---
+++ lib/faalis/dashboard/form_fields_properties.rb
@@ -0,0 +1,75 @@
+module Faalis::Dashboard
+  class FormFieldsProperties
+
+    def initializer
+      @fields = {}
+    end
+
+    def fields=(all_fields)
+      fields.each do |name, field|
+        unless all_fields.include? name.to_s
+          raise ArgumentError, "'#{name}' is not a valid field"
+        end
+      end
+
+      all_fields.each do |name, field|
+        unless fields.include?(name.to_sym)
+          send("setup_#{field.type}", name)
+        end
+      end
+    end
+
+    def fields
+      @fields || {}
+    end
+
+    def []=(name, detail)
+      @fields ||= {}
+      @fields[name.to_sym] = detail
+    end
+
+    def [](index)
+      unless @fields.nil?
+        return @fields[index] || {}
+      end
+      {}
+    end
+
+    def method_missing(m, *args, &block)
+      @fields ||= {}
+      if self.respond_to? m
+        return self.send(m, *args, &block)
+      else
+        # FIXME: This probably is a bad idea. We should get the whole
+        # args
+        @fields[m] = args[0]
+      end
+    end
+
+    def to_hash
+      fields
+    end
+
+    private
+
+      def setup_datetime(name)
+        self[name] = { as: :string,
+                       input_html: { class: 'datetimepicker' }}
+
+      end
+
+      def setup_time(name)
+        self[name] = { as: :string,
+                       input_html: { class: 'timepicker' }}
+
+      end
+
+      def setup_date(name)
+        self[name] = { as: :string,
+                       input_html: { class: 'datepicker' }}
+
+      end
+
+
+  end
+end
---
+++ lib/faalis/dashboard/new_dsl.rb
@@ -0,0 +1,94 @@
+module Faalis::Dashboard
+  module NewDSL
+
+    extend ActiveSupport::Concern
+
+    # The actual action method of a dashboard controller
+    def new
+      authorize model
+      setup_named_routes
+
+      collect_model_fields
+
+      @resource           = model.new
+      @resource_title     = _resource_title.singularize
+      @_fields_properties = _form_fields
+      @_fields_properties.fields = all_valid_columns
+
+      return if _override_views.include? :new
+      render 'faalis/dashboard/resource/new'
+    end
+
+    protected
+
+      def collect_model_fields
+        @_fields = {}
+        valid_columns = all_valid_columns
+
+        _new_form_fields.each do |name|
+          if valid_columns.keys.include? name.to_s
+            @_fields[name.to_sym] = valid_columns[name.to_s]
+          else
+            raise ArgumentError, "can't find '#{name}' field."
+          end
+        end
+      end
+
+    private
+
+      def _form_fields
+        form = ::Faalis::Dashboard::FormFieldsProperties.new
+        form.fields = all_valid_columns
+        form
+      end
+
+      def all_valid_columns
+        return @all_valid_columns unless @all_valid_columns.nil?
+        columns   = model.columns_hash
+        relations = model.reflections
+
+        relations.keys.each do |name|
+          col    = relations[name]
+          column = columns.delete col.foreign_key
+          columns[name] = column
+        end
+
+        columns.delete('id')
+
+        @all_valid_columns = columns
+      end
+
+      def _new_form_fields
+        all_valid_columns.keys.map(&:to_sym)
+      end
+
+    # The actual DSL for index ages
+    module ClassMethods
+      # User can provides the fields that he/she wants to be shown
+      # in the form for resource creation page.
+      # for example:
+      #
+      # class Dashboard::PostsController < Dashboard::ApplicationController
+      #   new_form_fields :title, created_at
+      # end
+      def new_form_fields(*fields)
+        define_method(:_new_form_fields) do
+          fields.map(&:to_sym)
+        end
+
+        private :_new_form_fields
+      end
+
+      def form_fields
+        form = ::Faalis::Dashboard::FormFieldsProperties.new
+        yield form
+
+        define_method(:_form_fields) do
+          form
+        end
+
+        private :_form_fields
+      end
+    end
+  end
+end
