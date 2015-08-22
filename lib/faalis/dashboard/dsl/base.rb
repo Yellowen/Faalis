3e38d3dfcce7afa05011b50849138ea8fb6d20d8
Sameer Rahmani <lxsameer@gnu.org>
2015-08-22 16:35:14 +0430

    DSL base class improved

--- lib/faalis/dashboard/dsl/base.rb
+++ lib/faalis/dashboard/dsl/base.rb
@@ -1,8 +1,19 @@
 module Faalis::Dashboard::DSL
   class Base
     attr_reader :action_buttons
+    attr_reader :fields
 
-    def action_button(:name, **options, &block)
+    # Define a new action button to be added to the
+    # action_buttons list on the corresponding view.
+    def action_button(name, **options, &block)
+      button = { name: name }
+      button.merge! options
+      button.merge!(block.call) if block_given?
+      @action_buttons[name] = button
+    end
+
+    def attributes(*field_names, **options, &block)
+
     end
   end
 end
