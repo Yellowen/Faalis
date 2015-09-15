e67e87ecb2e8965f57a20f2e1ca6de8271ab6a1c
Sameer Rahmani <lxsameer@gnu.org>
2015-09-15 18:18:27 +0430

    dashboard/application controller added to install generator

--- lib/generators/faalis/install_generator.rb
+++ lib/generators/faalis/install_generator.rb
@@ -39,6 +39,7 @@ module Faalis
         template 'faalis.rb', 'config/initializers/faalis.rb'
         template 'seeds.rb', 'db/seeds.rb'
         template 'api_controller.rb', 'app/controllers/api_controller.rb'
+        template 'dashboard_controller.rb', 'app/controllers/dashboard/application_controller.rb'
         template 'policy/application_policy.rb', 'app/policies/application_policy.rb'
       end
 
