c4370520b192fcad06ed2f8e9e8c5e21c611cc17
Sameer Rahmani <lxsameer@gnu.org>
2015-10-07 15:34:08 +0330

    A fork of rails guide added to faalis

---
+++ docs/guides/rails_guides.rb
@@ -0,0 +1,17 @@
+pwd = File.dirname(__FILE__)
+$:.unshift pwd
+
+begin
+  # Guides generation in the Rails repo.
+  as_lib = File.join(pwd, "../activesupport/lib")
+  ap_lib = File.join(pwd, "../actionpack/lib")
+
+  $:.unshift as_lib if File.directory?(as_lib)
+  $:.unshift ap_lib if File.directory?(ap_lib)
+rescue LoadError
+  # Guides generation from gems.
+  gem "actionpack", '>= 3.0'
+end
+
+require "rails_guides/generator"
+RailsGuides::Generator.new.generate
