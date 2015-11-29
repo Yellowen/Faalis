4e3f4ac2c88dbf4ff85b979fbf14ea066c1c1a6f
Sameer Rahmani <lxsameer@gnu.org>
2015-11-29 17:28:08 +0330

    before_create_hook added to dashboard create dsl

--- lib/faalis/fake_assets.rb
+++ lib/faalis/fake_assets.rb
@@ -1,7 +1,5 @@
 require 'rails-assets-sugar'
 require 'jquery-rails'
-#require 'dashstrap'
-
 require 'admin_lte-rails'
-require 'formtastic-bootstrap'
 require 'nprogress-rails'
+require 'formtastic-bootstrap'
