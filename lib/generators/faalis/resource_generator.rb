0207da44ba281ba490899ecc80d0bcab203dbb92
Sameer Rahmani <lxsameer@gnu.org>
2015-12-11 01:02:13 +0330

    Resource generator namespace issue fixed. close #50

--- lib/generators/faalis/resource_generator.rb
+++ lib/generators/faalis/resource_generator.rb
@@ -5,7 +5,6 @@ module Faalis
 
       desc 'Generates new Faalis resource.'
       argument :resource_name , type: :string, required: true
-      class_option :namespace, desc: 'The parent namespace'
 
       source_root File.expand_path('../templates', __FILE__)
 
@@ -15,7 +14,7 @@ module Faalis
 
       def create_controller
         template('dashboard/controller.rb.erb',
-                 "app/controllers/#{namespace.underscore + '/'}dashboard/#{resources}_controller.rb")
+                 "app/controllers/#{module_path}dashboard/#{resources}_controller.rb")
       end
 
       def inject_routes
@@ -24,18 +23,47 @@ module Faalis
                   "in_dashboard do\n    resources :#{resources}")
       end
 
+      def warning
+        unless module_name.blank?
+          puts ""
+          puts "[Warning] : Please fix your routes. Since you're using a resource inside"
+          puts "            a namespace, you have to fixed your routes to point to your"
+          puts "            new generate code. For example if you create a resource like"
+          puts "            `School::Student`, then you should have your route like this:"
+          puts ""
+          puts "            in_dashboard do"
+          puts "               scope :school do"
+          puts "                  resources students"
+          puts "               end"
+          puts "            end"
+          puts ""
+        end
+      end
       private
 
+      def module_name
+        resource_name.underscore.split('/')[0..-2].join('/')
+      end
+
+      def module_path
+        if !module_name.blank?
+          module_name + '/'
+        else
+          ''
+        end
+      end
+
+      def controller_module
+        path = module_path + 'dashboard/'
+        path.classify
+      end
+
       def resource
-        resource_name.singularize.underscore
+        resource_name.underscore.split('/')[-1].singularize
       end
 
       def resources
-        resource_name.pluralize.underscore
-      end
-
-      def namespace
-        options["namespace"] || ''
+        resource.pluralize
       end
     end
   end
