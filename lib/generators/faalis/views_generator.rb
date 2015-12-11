5cd3af8da63629238ff57cd7f7edaa545e05c84e
Sameer Rahmani <lxsameer@gnu.org>
2015-12-11 21:47:43 +0330

    Views generator added to copy create resource views. close #44

--- lib/generators/faalis/views_generator.rb
+++ lib/generators/faalis/views_generator.rb
@@ -1,43 +1,57 @@
-# -----------------------------------------------------------------------------
-#    Faalis - Basic website skel engine
-#    Copyright (C) 2012-2013 Yellowen
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-# -----------------------------------------------------------------------------
-
 module Faalis
   module Generators
     class ViewsGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../../app/', __FILE__)
 
-      desc "Copy all templates and assets to main application"
+      desc "Copy given views of a resource to the main application"
+      argument :resource_name , type: :string, required: true
+      argument :views_to_copy , type: :array, default: []
 
-      def copy_views_file
-        directory "views/faalis", "app/views/faalis"
-        directory "views/layouts/faalis", "app/views/layouts/faalis"
-        directory "views/angular", "app/views/layouts/faalis"
+      def copy_views
+        if views_to_copy.empty?
+          views = ['index', 'show', 'new', 'edit', 'create', 'update', 'destroy']
+        else
+          views = views_to_copy
+        end
 
+        empty_directory "app/views/#{resources}"
+
+        if views.include? 'index'
+          copy_file 'views/faalis/dashboard/resource/index.html.slim', "app/views/#{resources}/index.html.slim"
+        end
+
+        if views.include? 'show'
+          copy_file 'views/faalis/dashboard/resource/show.html.slim', "app/views/#{resources}/show.html.slim"
+        end
+
+        if views.include? 'new'
+          copy_file 'views/faalis/dashboard/resource/new.html.slim', "app/views/#{resources}/new.html.slim"
+          copy_file 'views/faalis/dashboard/resource/_form.html.slim', "app/views/#{resources}/_form.html.slim"
+        end
+
+        if views.include? 'edit'
+          copy_file 'views/faalis/dashboard/resource/edit.html.slim', "app/views/#{resources}/edit.html.slim"
+          copy_file 'views/faalis/dashboard/resource/_form.html.slim', "app/views/#{resources}/_form.html.slim"
+        end
+
+        if views.include? 'create'
+          copy_file 'views/faalis/dashboard/resource/create.js.erb', "app/views/#{resources}/create.js.erb"
+        end
+
+        if views.include? 'update'
+          copy_file 'views/faalis/dashboard/resource/update.js.erb', "app/views/#{resources}/update.js.erb"
+        end
+
+        if views.include? 'destroy'
+          copy_file 'views/faalis/dashboard/resource/destroy.js.erb', "app/views/#{resources}/destroy.js.erb"
+        end
       end
 
-      def copy_assets_file
-        directory "assets/javascripts/faalis", "app/assets/javascripts/faalis"
-        directory "assets/stylesheets/faalis", "app/assets/stylesheets/faalis"
-        directory "assets/images/faalis", "app/assets/images/faalis"
+      private
 
+      def resources
+        resource_name.underscore.pluralize
       end
-
     end
   end
 end
