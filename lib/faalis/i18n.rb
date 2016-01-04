ee7183643ad2e3ca1d3fc065229bb18f3440ca4e
Sameer Rahmani <lxsameer@gnu.org>
2016-01-04 11:59:57 +0330

    Rails i18n patch added to remove the span wrapping, we have to wait for rails5 for this

--- lib/faalis/i18n.rb
+++ lib/faalis/i18n.rb
@@ -16,64 +16,64 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
-# module ::ActionView
-#   # = Action View Translation Helpers
-#   module Helpers
-#     module TranslationHelper
+module ::ActionView
+  # = Action View Translation Helpers
+  module Helpers
+    module TranslationHelper
 
-#       def t(key, options = {})
-#         options = options.dup
-#         has_default = options.has_key?(:default)
-#         remaining_defaults = Array(options.delete(:default)).compact
+      def t(key, options = {})
+        options = options.dup
+        has_default = options.has_key?(:default)
+        remaining_defaults = Array(options.delete(:default)).compact
 
-#         if has_default && !remaining_defaults.first.kind_of?(Symbol)
-#           options[:default] = remaining_defaults
-#         end
-#         # If the user has explicitly decided to NOT raise errors, pass that option to I18n.
-#         # Otherwise, tell I18n to raise an exception, which we rescue further in this method.
-#         # Note: `raise_error` refers to us re-raising the error in this method. I18n is forced to raise by default.
-#         if options[:raise] == false
-#           raise_error = false
-#           i18n_raise = false
-#         else
-#           raise_error = options[:raise] || ActionView::Base.raise_on_missing_translations
-#           i18n_raise = true
-#         end
+        if has_default && !remaining_defaults.first.kind_of?(Symbol)
+          options[:default] = remaining_defaults
+        end
+        # If the user has explicitly decided to NOT raise errors, pass that option to I18n.
+        # Otherwise, tell I18n to raise an exception, which we rescue further in this method.
+        # Note: `raise_error` refers to us re-raising the error in this method. I18n is forced to raise by default.
+        if options[:raise] == false
+          raise_error = false
+          i18n_raise = false
+        else
+          raise_error = options[:raise] || ActionView::Base.raise_on_missing_translations
+          i18n_raise = true
+        end
 
-#         if html_safe_translation_key?(key)
-#           html_safe_options = options.dup
-#           options.except(*I18n::RESERVED_KEYS).each do |name, value|
-#             unless name == :count && value.is_a?(Numeric)
-#               html_safe_options[name] = ERB::Util.html_escape(value.to_s)
-#             end
-#           end
-#           translation = I18n.translate(scope_key_by_partial(key), html_safe_options.merge(raise: i18n_raise))
+        if html_safe_translation_key?(key)
+          html_safe_options = options.dup
+          options.except(*I18n::RESERVED_KEYS).each do |name, value|
+            unless name == :count && value.is_a?(Numeric)
+              html_safe_options[name] = ERB::Util.html_escape(value.to_s)
+            end
+          end
+          translation = I18n.translate(scope_key_by_partial(key), html_safe_options.merge(raise: i18n_raise))
 
-#           translation.respond_to?(:html_safe) ? translation.html_safe : translation
-#         else
-#           I18n.translate(scope_key_by_partial(key), options.merge(raise: i18n_raise))
-#         end
-#       rescue I18n::MissingTranslationData => e
-#         if remaining_defaults.present?
-#           translate remaining_defaults.shift, options.merge(default: remaining_defaults)
-#         else
-#           raise e if raise_error
+          translation.respond_to?(:html_safe) ? translation.html_safe : translation
+        else
+          I18n.translate(scope_key_by_partial(key), options.merge(raise: i18n_raise))
+        end
+      rescue I18n::MissingTranslationData => e
+        if remaining_defaults.present?
+          translate remaining_defaults.shift, options.merge(default: remaining_defaults)
+        else
+          raise e if raise_error
 
-#           keys = I18n.normalize_keys(e.locale, e.key, e.options[:scope])
-#           title = "translation missing: #{keys.join('.')}"
+          keys = I18n.normalize_keys(e.locale, e.key, e.options[:scope])
+          title = "translation missing: #{keys.join('.')}"
 
-#           interpolations = options.except(:default, :scope)
-#           if interpolations.any?
-#             title << ", " << interpolations.map { |k, v| "#{k}: #{ERB::Util.html_escape(v)}" }.join(', ')
-#           end
+          interpolations = options.except(:default, :scope)
+          if interpolations.any?
+            title << ", " << interpolations.map { |k, v| "#{k}: #{ERB::Util.html_escape(v)}" }.join(', ')
+          end
 
-#           content_tag('span', keys.last.to_s.titleize, class: 'translation_missing', title: title)
-#           #title
-#         end
-#       end
-#     end
-#   end
-# end
+          #content_tag('span', keys.last.to_s.titleize, class: 'translation_missing', title: title)
+          title
+        end
+      end
+    end
+  end
+end
 
 module Faalis
   # I18n related utility functions
