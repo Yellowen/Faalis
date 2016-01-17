db787acee675a45ce5cf0bb011dadbd9f59c3e3d
Sameer Rahmani <lxsameer@gnu.org>
2016-01-17 17:14:11 +0330

    tabbed_box helper added to dashboard

--- lib/faalis/dashboard/helpers/box_helpers.rb
+++ lib/faalis/dashboard/helpers/box_helpers.rb
@@ -21,6 +21,45 @@ module Faalis::Dashboard
         end
       end
 
+      class Tabs
+
+        class Tab
+          attr_reader :title, :title_icon
+          attr_accessor :content, :id
+
+          def initialize(title, **options)
+            @title      = title
+            @active     = false
+            @active     = true if options[:active] == true
+            @title_icon = options[:title_icon]
+          end
+
+          def active?
+            @active
+          end
+        end
+
+        attr_reader :title_icon
+
+        def initialize(**options)
+          @tabs       = []
+          @title_icon = options[:title_icon]
+        end
+
+
+        def tab(title, **options, &block)
+          id  = @tabs.length
+          tab = Tab.new(title, **options)
+          tab.content = block
+          tab.id = id
+          @tabs << tab
+        end
+
+        def tabs
+          @tabs
+        end
+      end
+
       def box(title = nil, **options)
         content = Box.new
 
@@ -30,7 +69,7 @@ module Faalis::Dashboard
           "<div class='box-header with-border'>" +
           "<h3 class='box-title'>" +
           title +
-          "</h3>"
+          '</h3>'
 
         unless options[:show_tools].nil?
           result += "<div class='box-tools pull-right'>" +
@@ -52,6 +91,37 @@ module Faalis::Dashboard
 
         result += "</div><!-- /.box -->"
 
+        result.html_safe
+      end
+
+
+      def tabbed_box(title = nil, **options)
+        content = Tabs.new
+
+        yield content
+
+        result = "<div class='nav-tabs-custom'>" +
+          "<ul class='nav nav-tabs pull-right'>"
+
+        content.tabs.each do |tab|
+          active = nil
+          active = "class='active'" if tab.active?
+
+          result += "<li #{active}><a href='#tab_#{tab.id}-#{tab.id}' data-toggle='tab'>" +
+            "<i class='fa #{tab.title_icon}'></i> #{tab.title}</a></li>"
+        end
+
+        result += "<li class='pull-left header'><i class='fa #{options[:title_icon]}'></i>#{title}</li>" +
+          "</ul>" +
+          "<div class='tab-content'>"
+
+        content.tabs.each do |tab|
+          result += "<div class='tab-pane #{'active' if tab.active?}' id='tab_#{tab.id}-#{tab.id}'>" +
+            capture(&tab.content) +
+            "</div><!-- /.tab-pane -->"
+        end
+
+        result += "</div><!-- /.tab-content --></div>"
         result.html_safe
       end
     end
