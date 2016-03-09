d618978c3a300528d21dbbf3b3c96a0f53b66a73
Sameer Rahmani <lxsameer@gnu.org>
2016-03-09 00:23:08 -0800

    faalis configuration module bug fixed

--- lib/faalis/configuration.rb
+++ lib/faalis/configuration.rb
@@ -1,56 +1,58 @@
-module Faalis::Configuration
+module Faalis
+  module Configuration
 
-  # Configure logger
-  mattr_accessor :logger
-  @@logger = Logger.new(STDOUT)
+    # Configure logger
+    mattr_accessor :logger
+    @@logger = Logger.new(STDOUT)
 
-  # Dashboard url prefix
-  mattr_accessor :dashboard_namespace
-  @@dashboard_namespace = :dashboard
+    # Dashboard url prefix
+    mattr_accessor :dashboard_namespace
+    @@dashboard_namespace = :dashboard
 
-  # ==> ORM configuration
-  # Load and configure the ORM. Supports :active_record (default) and
-  # :mongoid (bson_ext recommended) by default. Other ORMs may be
-  # available as additional gems.
-  # ORM name to use. either 'active_record' or 'mongoid'
-  mattr_accessor :orm
+    # ==> ORM configuration
+    # Load and configure the ORM. Supports :active_record (default) and
+    # :mongoid (bson_ext recommended) by default. Other ORMs may be
+    # available as additional gems.
+    # ORM name to use. either 'active_record' or 'mongoid'
+    mattr_accessor :orm
 
-  def orm=(orm_name)
-    @@orm = orm_name
-    require "devise/orm/#{orm_name}"
-  end
-
-  # We have to move this method somewhere else
-  def collect_i18n_missing_keys=(value)
-    if value
-      ::I18n.exception_handler = Faalis::I18n::MissingKeyHandler.new
+    def orm=(orm_name)
+      @@orm = orm_name
+      require "devise/orm/#{orm_name}"
     end
+
+    # We have to move this method somewhere else
+    def collect_i18n_missing_keys=(value)
+      if value
+        ::I18n.exception_handler = Faalis::I18n::MissingKeyHandler.new
+      end
+    end
+
+    # Site Title
+    attr_accessor :site_title
+    @site_title = 'Faalis'
+
+    mattr_accessor :slug
+
+    # Dashboard default javascript manifest
+    mattr_accessor :dashboard_js_manifest
+    @@dashboard_js_manifest = 'dashboard/application.js'
+
+    # Devise options
+    # Include default devise modules. Others available are:
+    # :token_authenticatable, :confirmable,
+    # :lockable, :timeoutable and :omniauthable
+    mattr_accessor :devise_options
+    @@devise_options = [:database_authenticatable,
+                        :registerable,
+                        :recoverable,
+                        :rememberable,
+                        :trackable,
+                        :lockable,
+                        :timeoutable,
+                        :validatable]
+
+    mattr_accessor :devise_for
+    @@devise_for = {}
   end
-
-  # Site Title
-  attr_accessor :site_title
-  @site_title = 'Faalis'
-
-  mattr_accessor :slug
-
-  # Dashboard default javascript manifest
-  mattr_accessor :dashboard_js_manifest
-  @@dashboard_js_manifest = 'dashboard/application.js'
-
-  # Devise options
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  mattr_accessor :devise_options
-  @@devise_options = [:database_authenticatable,
-                      :registerable,
-                      :recoverable,
-                      :rememberable,
-                      :trackable,
-                      :lockable,
-                      :timeoutable,
-                      :validatable]
-
-  mattr_accessor :devise_for
-  @@devise_for = {}
 end
