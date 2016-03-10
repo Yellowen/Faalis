5e3e30a430495c4344aec6db3051863093241285
Sameer Rahmani <lxsameer@gnu.org>
2016-03-10 02:02:13 -0800

    rspec disabled temprory

--- lib/faalis/engine.rb
+++ lib/faalis/engine.rb
@@ -38,7 +38,7 @@ module Faalis
     end
 
     config.generators do |g|
-      g.test_framework      :rspec, fixture: false
+      #g.test_framework      :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
       g.integration_tool    :rspec
       g.assets              false
