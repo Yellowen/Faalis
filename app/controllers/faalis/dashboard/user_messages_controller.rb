22ae38b5812f28d9c81fb4b6d38c275947cdaa35
sadmad <sarmad.re@gmail.com>
2016-03-14 17:18:32 +0330

    translaed user message model

--- app/controllers/faalis/dashboard/user_messages_controller.rb
+++ app/controllers/faalis/dashboard/user_messages_controller.rb
@@ -1,6 +1,5 @@
 module Faalis::Dashboard
   class UserMessagesController < ::Dashboard::ApplicationController
-    def index
-    end
+
   end
 end
--- app/models/faalis/user_message.rb
+++ app/models/faalis/user_message.rb
@@ -1,6 +1,6 @@
 module Faalis
   class UserMessage < ActiveRecord::Base
-    belongs_to :senders, class_name: Faalis::User
-    belongs_to :recievers, class_name: Faalis::User
+    belongs_to :sender, class_name: 'Faalis::User'
+    belongs_to :reciever, class_name: 'Faalis::User'
   end
 end
