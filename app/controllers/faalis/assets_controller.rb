a23933b452a456af57fd86f59e57fdd25e230008
Sameer Rahmani <lxsameer@gnu.org>
2016-04-14 03:31:13 -0700

    PoC of AMD done. moving AMD functionality to an intependent gem

--- app/controllers/faalis/assets_controller.rb
+++ app/controllers/faalis/assets_controller.rb
@@ -1,9 +1,13 @@
 class Faalis::AssetsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def finder
+    path = "#{params[:asset]}.amd"
     # It's important that the arg passed to asset_path already
     # be in precompile list
-    module_path = ActionController::Base.helpers.asset_path(params[:asset])
+    module_path = ActionController::Base.helpers.asset_path(path,
+                                                            type: :javascript)
     redirect_to module_path
   end
 
