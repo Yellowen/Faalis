53776234887f3608398af56d01de9279fa5dc49d
Sameer Rahmani <lxsameer@gnu.org>
2016-05-24 15:22:55 +0430

    sprocket4 manifest file added

---
+++ app/assets/javascripts/faalis/dashboard/manifest.js
@@ -0,0 +1,5 @@
+//= link dashboard/application.js
+//= link  faalis/dashboard/ltr/application.css
+//= link  faalis/dashboard/rtl/application.css
+//= link  faalis/simple/ltr/application.css
+//= link  faalis/simple/rtl/application.css
