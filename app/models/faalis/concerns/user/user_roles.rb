edeac6ed433a78fbf45f690a479bb234803abb63
sadmad <sarmad.re@gmail.com>
2016-06-08 13:48:22 +0430

    Un-necessary old files removed

--- app/models/faalis/concerns/user/user_roles.rb
+++ app/models/faalis/concerns/user/user_roles.rb
@@ -1,20 +1,24 @@
 module Faalis
   # This concern contains all the role related methods
   # to use within `Faalis::User`
-  module Concerns::User::UserRoles
+  module Concerns
+    module User
+      module UserRoles
 
-    # Return an array of user roles.
-    def roles
-      self.groups.to_a.map(&:role)
-    end
+        # Return an array of user roles.
+        def roles
+          self.groups.to_a.map(&:role)
+        end
 
-    def role? role
-      roles.include? role.to_s
-    end
+        def role? role
+          roles.include? role.to_s
+        end
 
-    # A shortcut for `role? :admin`
-    def admin?
-      role? :admin
+        # A shortcut for `role? :admin`
+        def admin?
+          role? :admin
+        end
+      end
     end
   end
 end
--- app/models/faalis/permissions/auth.rb
+++
@@ -1,8 +0,0 @@
-class Faalis::Permissions::Auth
-  # Make this model authorizable
-  include Faalis::Concerns::Authorizable
-
-  def self.humanize_class_name
-    'Auth Module'
-  end
-end
