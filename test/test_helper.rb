a770e5f7e01f8b8617826935b0396a39365bed5a
Sameer Rahmani <lxsameer@gnu.org>
2017-07-09 21:16:44 +0100

    rubocop config added, travis configuration revised

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,7 +18,6 @@ Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new,
                          ENV,
                          Minitest.backtrace_filter)
 
-
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
@@ -26,25 +25,7 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
-
-
 # Fabrication.configure do |config|
 #   config.fabricator_path = 'test/fabricators/faalis/'
 #   config.path_prefix = Faalis::Engine.root
 # end
-
-
-
-# CONNECTION POOL --------------------------------
-# require 'connection_pool'
-
-# class ActiveRecord::Base
-#   mattr_accessor :shared_connection
-#   @@shared_connection = nil
-
-#   def self.connection
-#     @@shared_connection || ::ConnectionPool::Wrapper.new(:size => 1) { retrieve_connection }
-#   end
-# end
-
-# ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
